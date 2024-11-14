5a53a3b825c0600f613fb356bec5cbbd47742bac
ostapzhp <ostapzhp@gmail.com>
2024-11-14 17:07:08 -0600

    Small bug fixed

--- pages/components/LoadingComponent/LoadingComponent.tsx
+++ pages/components/LoadingComponent/LoadingComponent.tsx
@@ -1,9 +1,8 @@
-// components/LoadingComponent.js
 import { ClipLoader } from 'react-spinners';
 
 const LoadingComponent = () => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div className='flex justify-center h-[100vh] [alignItems:center]'>
       <ClipLoader color="#36d7b7" loading={true} size={50} />
     </div>
   );
