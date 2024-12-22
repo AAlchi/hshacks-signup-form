fc646228884358c50cf11c97ba6029015c771f7d
Ali Alchikh Ibrahim <alialchikhibrahim@Alis-MacBook-Pro.local>
2024-12-21 18:41:09 -0600

    Changes part 1 to the frontend

--- pages/components/buttonComponent/ButtonComponent.tsx
+++ pages/components/buttonComponent/ButtonComponent.tsx
@@ -3,26 +3,32 @@ import React from 'react';
 interface ButtonComponentProps {
   name: string;
   secondary?: boolean;
-  onClick?: () => void;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
   disabled?: boolean;
+  submit?: boolean;
 }
 
 const ButtonComponent: React.FC<ButtonComponentProps> = ({
   name,
   secondary,
   onClick,
-  disabled
+  disabled,
+  submit
 }) => {
-  const handleSubmit = () => { 
-    if (onClick) {
-      onClick();
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => { 
+    if (!disabled) {
+      if (onClick) {
+        onClick(e);
+      }
+      window.scrollTo(0, 0);
+    } else {
+     e.preventDefault()
     }
-    window.scrollTo(0, 0);
   };
   
 
   return (
-    <button onClick={handleSubmit} type="submit" className={`${secondary ? "bg-slate-500" : "bg-blue-500"} ${disabled && "opacity-50 cursor-block"} w-full text-white p-2 rounded hover:opacity-50 transition px-4`}>
+    <button onClick={handleSubmit} type={submit ? "submit" : "button"} className={`${secondary ? "bg-slate-500" : "bg-blue-500"} ${disabled && "opacity-50 cursor-block"} w-full text-white p-2 rounded hover:opacity-50 transition px-4`}>
       {name}
     </button>
   );
--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,13 +1,15 @@
 import RadioComponent from "../components/RadioComponent/RadioComponent";
 import React, { useEffect, useState } from "react";
-import { useRouter } from 'next/router'; 
-import TextFieldComponent from "../components/TextFieldComponent/TextFieldComponent"; 
+import { useRouter } from 'next/router';
+import TextFieldComponent from "../components/TextFieldComponent/TextFieldComponent";
 import TimeBoxComponent from "../components/TimeBoxComponent/TimeBoxComponent";
-import LoadingComponent from "../components/LoadingComponent/LoadingComponent"; 
-import ButtonComponent from "../components/buttonComponent/ButtonComponent"; 
+import LoadingComponent from "../components/LoadingComponent/LoadingComponent";
+import ButtonComponent from "../components/buttonComponent/ButtonComponent";
 import InformationComponent from "./InformationComponent";
 import axios from "axios";
 import { toast } from "react-toastify";
+import Link from "next/link";
+import ReCAPTCHA from 'react-google-recaptcha';
 
 interface FormData {
   firstName: string;
@@ -23,8 +25,8 @@ interface FormData {
 
 
 const Register = () => {
-  const router = useRouter(); 
-  const [step, setStep] = useState(0); 
+  const router = useRouter();
+  const [step, setStep] = useState(0);
   const [formData, setFormData] = useState<FormData>({
     "firstName": "",
     "lastName": "",
@@ -45,7 +47,7 @@ const Register = () => {
         const response = await axios.get('/api/registrationFields');
         setFormFields(response.data);
         setIsLoading(false);
-      } catch (error) { 
+      } catch (error) {
         toast.error('There was an error, please try again later.', {
           position: "top-center",
           autoClose: 5000,
@@ -54,8 +56,8 @@ const Register = () => {
           pauseOnHover: true,
           draggable: true,
           progress: undefined,
-          theme: "light", 
-          });
+          theme: "light",
+        });
       }
     };
 
@@ -64,72 +66,96 @@ const Register = () => {
 
   const redirect = () => {
     router.push("./RegistrationComplete");
-  }; 
- 
+  };
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;  
- 
+    const { name, value } = e.target;
+
     setFormData((prevFormData) => ({
-      ...prevFormData,  
-      [name]: value  
+      ...prevFormData,
+      [name]: value
     }));
+  };
 
-    
-  }; 
-
-  const nextStep = (e: any) => {
+  const handlePreviousStep = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
-    
-    if (step < 3) {
-      setStep(step + 1)
-    } else {
-      redirect()
-    }
+
+    setStep(step - 1)
   }
 
+  const handleNextStep = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+    if (step + 1 == 3) {
+      redirect;
+    } else {
+      e.preventDefault();
+      setStep(step + 1);
+    }
+
+  }
+
+  const [recaptchaValue, setRecaptchaValue] = useState(null);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://www.google.com/recaptcha/enterprise.js?render=';
+    script.async = true;
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
+  const handleRecaptchaChange = (value: any) => {
+    setRecaptchaValue(value);
+  };
   return (
-      <div className="flex items-center justify-center bg-none relative py-10" style={{overflow: "hidden"}}>
-      <div className="bg-white flex flex-col rounded w-[600px] p-6 mt-[85px] relative">
+    <div className="flex items-center justify-center bg-none relative py-10" style={{ overflow: "hidden" }}>
+      <div className="bg-white flex flex-col rounded w-11/12 p-6 mt-[85px] relative max-w-[600px]">
         <div className="flex items-center justify-start">
           <img style={{ width: "200px" }} src="./HSHacks_Logo.png" alt="HSHacks_Logo" />
-          <p className="text-2xl pl-2 text-black">| Registration</p>
-        </div>   
-          <InformationComponent name="General Information" />
-          {!isLoading ? ( 
-              <form onSubmit={nextStep}>
-                {formFields.map((data: any) => ( 
-                  <div key={data.props.name}>
-                    {data.component == "TextFieldComponent" && data.group == step && (
-                      <TextFieldComponent required={data.required} name={data.props.name} placeHolder={data.props.placeHolder} question={data.props.question} onChange={(e) => handleInputChange(e)} type={data.type} value={formData[data.props.name as keyof FormData]} />
-                    )}
-                    {data.component == "RadioComponent" && data.group == step && (
-                      <RadioComponent required={data.required} name={data.props.name} listOfNames={data.props.listOfNames} question={data.props.question} onChange={(e) => handleInputChange(e)} chosenElement={formData[data.props.name as keyof FormData]} />
-                    )}
-                  </div>
-                ))}
-                {step == 0 ? (
-                  <ButtonComponent name="Next" />
-                ) : step < 2 ? (
-                  <div className="flex w-full gap-2">
-                    <ButtonComponent name="Back" secondary onClick={() => setStep(step - 1)} />
-                    <ButtonComponent name="Next" />
-                  </div>
-                ) : (
-                  <div className="flex w-full gap-2">
-                    <ButtonComponent name="Back" secondary onClick={() => setStep(step - 1)}/>
-                    <ButtonComponent name="Submit" />
-                  </div>
+        </div>
+        <InformationComponent name={step == 0 ? "General Information" : step == 1 ? "Event Information" : "Other Information"} />
+        {!isLoading ? (
+          <form onSubmit={(e) => handleNextStep(e as any)}>
+            {formFields.map((data: any) => (
+              <div key={data.props.name}>
+                {data.component == "TextFieldComponent" && data.group == step && (
+                  <TextFieldComponent required={data.required} name={data.props.name} subtext={data.props.subtext} placeHolder={data.props.placeHolder} question={data.props.question} onChange={(e) => handleInputChange(e)} type={data.type} value={formData[data.props.name as keyof FormData]} />
                 )}
-              </form>  
-          ) : (
-            <LoadingComponent />
-          )}
-           
+                {data.component == "RadioComponent" && data.group == step && (
+                  <RadioComponent required={data.required} name={data.props.name} subtext={data.props.subtext} listOfNames={data.props.listOfNames} question={data.props.question} onChange={(e) => handleInputChange(e)} chosenElement={formData[data.props.name as keyof FormData]} />
+                )}
+              </div>
+            ))}
+
+
+            {step == 2 && (
+              <div className="flex flex-col gap-1">
+                <ReCAPTCHA
+                  sitekey=""
+                  onChange={handleRecaptchaChange}
+                />
+                <p className="text-slate-500 text-sm pb-2">Please read over the <Link style={{ color: "blue" }} href="/codeofconduct" target="_blank">HSHacks code of conduct</Link> before submiting</p>
+              </div>
+            )}
+            <div className="flex gap-2 w-full">
+              {step != 0 && (
+                <ButtonComponent secondary name="Back" onClick={(e) => handlePreviousStep(e as any)} />
+              )}
+              <ButtonComponent name={step == 2 ? "Submit" : "Next"} submit disabled={step == 2 && !recaptchaValue} />
+            </div> 
+          </form>
+        ) : (
+          <LoadingComponent />
+        )}
+
       </div>
       <div className="invisible lg:visible absolute bottom-3 right-3"><TimeBoxComponent /></div>
 
 
-    </div> 
+    </div>
   )
 
 }
